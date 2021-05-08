e3d7b33ebc6a99b2160e994f488d5f967c212016
AnuraghSarkar <anuraghtimalsina@gmail.com>
2021-05-08 10:34:24 +0545

    v-model to :value and @ is at that eveent

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const vm = Vue.createApp({
     return {
       firstName: "das",
       lastName: "dsad",
-        url: 'https://youtube.com',
-      rawUrl : '<a :href="https://youtube.com" target="_blank">Youtube</a>',
+      url: "https://youtube.com",
+      rawUrl: '<a :href="https://youtube.com" target="_blank">Youtube</a>',
+      age: 20,
     };
   },
   methods: {
     fullName() {
       return `${this.firstName} ${this.lastName.toUpperCase()}`;
     },
+    increment() {
+      this.age++;
+      },
+      updateLastName(event) {
+        this.lastName = event.target.value
+       }
   },
 }).mount("#app");
